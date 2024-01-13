3e272f59222e088a5c14833c9f5ca5de909bf9d8
Flávio <f.augustosdn@gmail.com>
2024-01-12 22:42:46 -0300

    feat: finalizada estilização da tela de home e definido grid

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,9 +8,49 @@ export function Home() {
     <Container>
       <div>
         <h1>As maiores galáxias</h1>
-        <p>E curiosidades sobre elas</p>
+        <h2>E curiosidades sobre elas</h2>
 
         <section>
+          <Card
+            image={IC1101}
+            title={"IC 1101"}
+            description={
+              "A IC 1101 é a maior galáxia do universo. A galáxia é elíptica à lenticular e aparece como coloração amarelo dourado. A galáxia mais brilhante está situada a mais de 1.04 bilhões de anos-luz da Terra. "
+            }
+          />
+
+          <Card
+            image={IC1101}
+            title={"IC 1101"}
+            description={
+              "A IC 1101 é a maior galáxia do universo. A galáxia é elíptica à lenticular e aparece como coloração amarelo dourado. A galáxia mais brilhante está situada a mais de 1.04 bilhões de anos-luz da Terra. "
+            }
+          />
+
+          <Card
+            image={IC1101}
+            title={"IC 1101"}
+            description={
+              "A IC 1101 é a maior galáxia do universo. A galáxia é elíptica à lenticular e aparece como coloração amarelo dourado. A galáxia mais brilhante está situada a mais de 1.04 bilhões de anos-luz da Terra. "
+            }
+          />
+
+          <Card
+            image={IC1101}
+            title={"IC 1101"}
+            description={
+              "A IC 1101 é a maior galáxia do universo. A galáxia é elíptica à lenticular e aparece como coloração amarelo dourado. A galáxia mais brilhante está situada a mais de 1.04 bilhões de anos-luz da Terra. "
+            }
+          />
+
+          <Card
+            image={IC1101}
+            title={"IC 1101"}
+            description={
+              "A IC 1101 é a maior galáxia do universo. A galáxia é elíptica à lenticular e aparece como coloração amarelo dourado. A galáxia mais brilhante está situada a mais de 1.04 bilhões de anos-luz da Terra. "
+            }
+          />
+
           <Card
             image={IC1101}
             title={"IC 1101"}
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,3 +1,28 @@
 import styled from "styled-components";
 
-export const Container = styled.main``;
+export const Container = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  margin-top: 7.3rem;
+  margin-bottom: 11.4rem;
+
+  h1 {
+    color: #02799D;
+    font-size: 5.8rem;
+  }
+  
+  h2 {
+    color: #E1E1E6;
+    font-size: 3.2rem;
+    margin-bottom: 6.3rem;
+  }
+
+  section {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    row-gap: 4.4rem;
+    column-gap: 6rem;
+  }
+`;
